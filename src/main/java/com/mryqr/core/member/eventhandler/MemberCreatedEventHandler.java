d949830492bf7cc29729c43058c15b4b92707315
Teng Yun <davenkin@126.com>
2024-05-17 00:24:37 +0800

    moved count member for tenant task to tenant package

--- src/main/java/com/mryqr/core/member/eventhandler/MemberCreatedEventHandler.java
+++ src/main/java/com/mryqr/core/member/eventhandler/MemberCreatedEventHandler.java
@@ -4,8 +4,12 @@ import com.mryqr.core.common.domain.event.DomainEvent;
 import com.mryqr.core.common.domain.event.DomainEventHandler;
 import com.mryqr.core.common.utils.MryTaskRunner;
 import com.mryqr.core.member.domain.event.MemberCreatedEvent;
+<<<<<<< HEAD
 import com.mryqr.core.member.domain.task.CountMembersForTenantTask;
 import com.mryqr.core.member.domain.task.DeltaCountMemberForTenantTask;
+=======
+import com.mryqr.core.tenant.domain.task.CountMembersForTenantTask;
+>>>>>>> 65d510a (moved count member for tenant task to tenant package)
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
